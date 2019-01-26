c3fa7f14a1211162c860441a4bf01133fd52ef74
leslie-alldridge <leslie.alldridge@gmail.com>
2019-01-26 15:25:19 +1300

    removing store

--- client/index.js
+++ client/index.js
@@ -7,13 +7,7 @@ import thunkMiddleware from "redux-thunk";
 import reducers from "./reducers";
 import App from "./components/App";
 
-let store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunkMiddleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )
-);
+let store = createStore(reducers, compose(applyMiddleware(thunkMiddleware)));
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
